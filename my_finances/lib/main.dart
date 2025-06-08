18b22b66fddef91c349795a173625bd7e5a3da5b
Lorenzo Janota <lorenzo.janota@icloud.com>
2025-06-08 12:14:45 -0300

    Versão Final

--- my_finances/lib/main.dart
+++ my_finances/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'screens/login_screen.dart';
+import 'screens/auth/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
