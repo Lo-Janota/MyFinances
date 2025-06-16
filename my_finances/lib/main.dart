cc79ce421a8e9def06ec0cd0a6ffa2f93431d677
Lorenzo Janota <lorenzo.janota@icloud.com>
2025-06-15 22:53:15 -0300

    Melhorias ao logar.

--- my_finances/lib/main.dart
+++ my_finances/lib/main.dart
@@ -1,17 +1,17 @@
-import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-import 'screens/auth/login_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:my_finances/firebase_options.dart';
+import 'package:my_finances/screens/home/home_screen.dart';
+import 'package:my_finances/screens/auth/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
   runApp(
     DevicePreview(
       enabled: true,
@@ -27,21 +27,56 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My Finances',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
+        useMaterial3: true,
+      ),
       localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: const [
-        Locale('pt', 'BR'), // Suporte para Português do Brasil
+        Locale('pt', 'BR'),
       ],
       locale: const Locale('pt', 'BR'),
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
-        useMaterial3: true,
-      ),
-      home: LoginScreen(),
-      builder: DevicePreview.appBuilder,
+      home: const AuthCheck(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class AuthCheck extends StatelessWidget {
+  const AuthCheck({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      // Escuta em tempo real as mudanças de autenticação do Firebase
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        
+        // Se ainda está verificando, mostra uma tela de loading
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+
+        // Se o snapshot tem dados (User não é nulo), o usuário está logado
+        if (snapshot.hasData) {
+          // Direciona para a HomeScreen
+          return const HomeScreen();
+        }
+
+        // Se não tem dados (User é nulo), o usuário não está logado
+        else {
+          // Direciona para a LoginScreen
+          return const LoginScreen();
+        }
+      },
     );
   }
 }
